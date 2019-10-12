53ca2c903038d0037116df3356973a2afcc3f839
Douglas Silva <douglas_hss@hotmail.com.br>
2019-10-12 13:12:41 -0300

    Remove unused fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,20 +6,6 @@ from aws_sns_utils.client import SNSClient
 
 
 @pytest.fixture
-def mock_boto_client_sns():
-    topics = {
-        "Topics": [
-            {"TopicArn": "arn:aws:region:id:TEST__test1"},
-            {"TopicArn": "arn:aws:region:id:TEST__test2"},
-            {"TopicArn": "arn:aws:region:id:TEST2__test1"},
-            {"TopicArn": "arn:aws:region:id:test__test_status"},
-        ]
-    }
-    client = mock.Mock(list_topics=mock.Mock(return_value=topics), publish=mock.Mock())
-    return mock.patch("boto3.client", return_value=client)
-
-
-@pytest.fixture
 def sns_client():
     client = SNSClient()
     client.client = mock.Mock()
